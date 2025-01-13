3d741a441933da5606a51ba3e2ac10432157de04
Naveen-Sesetti <48952093+Naveen-Sesetti@users.noreply.github.com>
2025-01-13 20:08:23 +0000

    added vercel.jason

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'pages/index.html'));
 });
 
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 3000; 
 
 app.listen(port, ()=>{
     console.log('Server running on https://localhost:${port}');
